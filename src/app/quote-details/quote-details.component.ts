628fa9f6a188ba2784e84f324f381c577e9ccede
karuu-01 <brian.muchori@student.moringaschool.com>
2021-07-17 17:14:56 +0300

    add changes

---
+++ src/app/quote-details/quote-details.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
+@Component({
+  selector: 'app-quote-details',
+  templateUrl: './quote-details.component.html',
+  styleUrls: ['./quote-details.component.css']
+})
+export class QuoteDetailsComponent implements OnInit {
+  @Input() quote! : Quote;
+  @Output () isWritten = new EventEmitter<boolean>();
+
+  quoteWritten(written:boolean){
+    this.isWritten.emit(written);
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/quote.ts
@@ -0,0 +1,5 @@
+export class Quote {
+ showDescription!: boolean;
+ constructor(public id: number, public name: string, public description: string){
+ }
+ }
